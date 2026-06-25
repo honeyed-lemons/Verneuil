fab1ffbbeb5c524289a746c9565d5d96a79e3105
honeyed-lemons <alex91905@yahoo.com>
2026-06-24 23:29:13 -0400

    wuh

---
+++ build/moddev/runClient.cmd
@@ -0,0 +1,9 @@
+@echo off
+setlocal
+for /f "tokens=2 delims=:." %%x in ('chcp') do set _codepage=%%x
+chcp 65001>nul
+cd D:\User\Documents\Projects\Verneuil\run
+C:\Users\alex\.jdks\temurin-25.0.3\bin\java.exe @D:\User\Documents\Projects\Verneuil\build\moddev\clientRunClasspath.txt @D:\User\Documents\Projects\Verneuil\build\moddev\clientRunVmArgs.txt -Dfml.modFolders=verneuil%%%%D:\User\Documents\Projects\Verneuil\build\classes\java\main;verneuil%%%%D:\User\Documents\Projects\Verneuil\build\resources\main net.neoforged.devlaunch.Main @D:\User\Documents\Projects\Verneuil\build\moddev\clientRunProgramArgs.txt
+if not ERRORLEVEL 0 (  echo Minecraft failed with exit code %ERRORLEVEL%  pause)
+chcp %_codepage%>nul
+endlocal
